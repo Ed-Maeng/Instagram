fda41764344950a0b3e8bff73a4ea2fd48554b8c
Edward Maeng <emaeng@vmware.com>
2023-01-06 06:11:42 +0000

    Create Homepage, Posts, and Widgets

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,36 @@
-import { Box } from '@mui/material'
-import Navbar from 'scenes/navbar'
+import { Box, useMediaQuery } from '@mui/material';
+import { useSelector } from 'react-redux';
+import Navbar from 'scenes/navbar';
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from 'scenes/widgets/PostsWidget';
+import UserWidget from 'scenes/widgets/UserWidget';
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:100px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"} 
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   )
 }
